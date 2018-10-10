d7475cec06aa277b84686846db826b12ae6d7367
V10LET <violet.wmoon@gmail.com>
2018-10-10 17:45:25 -0500

    Done.

--- js/index.js
+++ js/index.js
@@ -1,2 +1,47 @@
-// using this file is optional
-// you can also load the code snippets in using your browser's console
+//-----> Change main title when clicked
+const main = document.querySelector('#main')
+main.addEventListener('click', function(event) {
+  main.innerHTML = 'I was clicked!'
+  main.style.color = 'violet'
+  main.style.fontSize = '6em'
+})
+
+//-----> Change input label to input value when someone presses enter
+const input = document.querySelector('input')
+input.addEventListener('keydown', function(e) {
+  // console.log(e.wich)
+  if (e.which === 13) {
+    const label = document.querySelector('label')
+    label.innerHTML = input.value + ':'
+  }
+})
+
+//-----> Bubble: moves in to out
+let divs = document.querySelectorAll('div');
+
+function bubble(e) {
+  console.log(this.firstChild.nodeValue.trim() + ' bubbled');
+}
+
+for (let i = 0; i < divs.length; i++) {
+  divs[i].addEventListener('click', bubble);
+}
+
+//-----> Capture: moves out to in
+function capture(e) {
+  console.log(this.firstChild.nodeValue.trim() + ' captured');
+}
+
+for (let i = 0; i < divs.length; i++) {
+  divs[i].addEventListener('click', capture, true);
+}
+
+//-----> stopPropogation: only allows first bubbled item to surface
+function bubble(e) {
+ e.stopPropagation();
+ console.log(this.firstChild.nodeValue.trim() + ' bubbled');
+}
+
+for (let i = 0; i < divs.length; i++) {
+ divs[i].addEventListener('click', bubble);
+}
